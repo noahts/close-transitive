1524494fd11bb6f08580e81857101accd4dbdcdd
Noah Torp-Smith <noah@torpsmith.net>
2015-09-04 13:01:44 +0200

    refactoring to make it all work

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@
  * Created by noahtorpsmith on 04/09/15.
  */
 
-module.exports = transitiveClose;
+var _ = require('lodash');
 
-var transitiveClose = function (inFun) {
+var transitiveCloseAux = function(evaluated, pending, inFun) {
+  if (pending.length < 1) {
+    return evaluated;
+  }
+  else {
+    var item = pending.pop();
+    var candidates = inFun(item);
+    var newCandidates = _.difference(candidates, evaluated);
+    for (var newCandidate in newCandidates) {
+      pending.push(newCandidates[newCandidate]);
+    }
+    evaluated.push(item);
+    evaluated = _.uniq(evaluated);
+    pending = _.uniq(pending);
+    return transitiveCloseAux(evaluated, pending, inFun);
+  }
+};
 
+var transitiveCloseImpl = function (inFun, startInt) {
+  return transitiveCloseAux([], [startInt], inFun);
 }
+
+module.exports = {
+  transitiveClose : transitiveCloseImpl
+};
